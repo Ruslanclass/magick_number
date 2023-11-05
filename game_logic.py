ea967049fb56361d5fec14ab6909aeed3dd7228e
Ward Vult <nikdol011102@gmail.com>
2023-11-05 16:18:18 +0200

    rand&func

--- game_logic.py
+++ game_logic.py
@@ -3,43 +3,55 @@ import PySimpleGUI as sg
 from database import load_settings, save_settings
 
 def magic_number_game():
-    attempts = 0
-
-    layout = [
-        [sg.Text("Вгадай число!", font=("Helvetica", 16))],
-        [sg.Text("Введіть число від 1 до 5: ")],
-        [sg.InputText(key="user_guess")],
-        [sg.Text("", size=(20, 2), key="output_text")],
-        [sg.Button("Вгадати"), sg.Button("Вийти")]
-    ]
-
-    window = sg.Window("Гра Вгадай число", layout)
-
     while True:
-        event, values = window.read()
+        attempts = 0
 
-        if event == sg.WIN_CLOSED or event == "Вийти":
-            break
+        layout = [
+            [sg.Text("Вгадай число!", font=("Helvetica", 16))],
+            [sg.Text("Введіть число від 1 до 5: ")],
+            [sg.InputText(key="user_guess")],
+            [sg.Text("", size=(20, 2), key="output_text")],
+            [sg.Button("Вгадати"), sg.Button("Вийти")]
+        ]
 
-        if event == "Вгадати":
-            user_guess = values["user_guess"]
+        window = sg.Window("Гра Вгадай число", layout)
 
-            try:
-                user_guess = int(user_guess)
-                attempts += 1
+        while True:
+            event, values = window.read()
 
-                magic_number = random.randint(1, 5)  # Генерувати нове число
+            if event == sg.WIN_CLOSED or event == "Вийти":
+                window.close()
+                return
 
-                if user_guess < magic_number:
-                    window["output_text"].update("Число більше.")
-                elif user_guess > magic_number:
-                    window["output_text"].update("Число менше.")
-                else:
-                    window["output_text"].update(f"Ви вгадали число {magic_number} за {attempts} спроб.")
-                    window["Вгадати"].update(disabled=True)  
-            except ValueError:
-                window["output_text"].update("Напишіть число в межах від 1 до 5.")
+            if event == "Вгадати":
+                user_guess = values["user_guess"]
 
-    window.close()
+                try:
+                    user_guess = int(user_guess)
+                    attempts += 1
 
-magic_number_game()
+                    magic_number = random.randint(1, 5)
+
+                    if user_guess < magic_number:
+                        window["output_text"].update("Число більше.")
+                    elif user_guess > magic_number:
+                        window["output_text"].update("Число менше.")
+                    else:
+                        window["output_text"].update(f"Ви вгадали число {magic_number} за {attempts} спроб.")
+                        window["Вгадати"].update(disabled=True)
+
+                        replay = sg.popup_yes_no("Бажаєте грати ще раз?", "Гра Вгадай число")
+
+                        if replay == "Yes":
+                            break
+                        else:
+                            window.close()
+                            return
+                except ValueError:
+                    window["output_text"].update("Напишіть число в межах від 1 до 5.")
+
+def main():
+    magic_number_game()
+
+if __name__ == "__main__":
+    main()
